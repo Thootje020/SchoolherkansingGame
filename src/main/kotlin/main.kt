12a07bd00c215b1744a11cadb6537602f37a0d85
Thootje020 <319878@student.mboutrecht.nl>
2021-06-30 12:17:21 +0200

    Change: Added username screen

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,18 @@ import kotlin.system.exitProcess
 fun main (args: Array<String>) {
     println("Welcome to our game")
 
+    var player = Player("")
+
+    var username = readLine()
+    player = Player("$username")
+    while (username!!.isBlank()) {
+        println("enter a valid username")
+        username = readLine()
+    }
+        println("Welcome," + player)
+
+
+
 }
 
 
