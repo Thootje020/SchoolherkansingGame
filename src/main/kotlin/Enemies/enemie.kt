6781c7834624db7c3ef1e7c61308c139e3728636
Thootje020 <319878@student.mboutrecht.nl>
2021-06-30 11:44:01 +0200

    Added enemey class So we can have enemies

---
+++ src/main/kotlin/Enemies/enemie.kt
@@ -0,0 +1,23 @@
+public enum class Enemy(var enemyname: String, var hp: Int) {
+    Shane("Shane", 10), Ron("Ron", 15)
+}
+
+public fun attackShane() {
+    if (Enemy.Shane.hp == 0) {
+    } else {
+        val attackDamage = (1..2).random();
+        Enemy.Shane.hp -=attackDamage
+    }
+}
+public fun attackRon() {
+    if (Enemy.Ron.hp == 0) {
+    } else {
+        val attackDamage = (1..2).random();
+        Enemy.Ron.hp -=attackDamage
+    }
+}
+
+public fun takeDamageShane() {
+}
+public fun takeDamageRon() {
+}
