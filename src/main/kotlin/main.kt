b78cae507bffa1ad7f2d983dfc05f1d34976768b
AronVLD <aronridder@hotmail.nl>
2021-06-30 12:06:57 +0200

    Added SuperClass and edited player class

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,11 @@
-
+import kotlin.system.exitProcess
 fun main (args: Array<String>) {
     println("Welcome to our game")
 
+}
+
+
+fun died(){
+    println("You have died and lost the game")
+    exitProcess(1)
 }
